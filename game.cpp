9586e2c595e5c3a75b9bf8919986106fce2ee424
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2022-08-15 19:48:47 +0200

    new

--- game.cpp
+++ game.cpp
@@ -1,44 +1,30 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-const int width = 800;
-const int height = 600;
+const int screen_width = 900;
+const int screen_height = 700;
 
-class obj {
+// Player class
+class player {
 	public:
-		obj();
+		int x = screen_width/2;
+		int y = screen_height-40;
+
 		void handleEvents(SDL_Event& e);
 		void move();
 		void render(SDL_Renderer* renderer);
 
 	private:
-		int x;
-		int y;
 		int vx;
-		int vy;
 		static const int width = 100;
-		static const int height = 100;
+		static const int height = 20;
 		static const int vel = 2;
 };
 
-obj::obj() {
-	x = 100;
-	y = 100;
-}
-
-void obj::handleEvents(SDL_Event& e) {
+void player::handleEvents(SDL_Event& e) {
 	vx = 0;
-	vy = 0;
-	if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+	if (e.type == SDL_KEYDOWN) {
 		switch (e.key.keysym.sym) {
-			case SDLK_UP:
-				vy -= vel;
-				break;
-
-			case SDLK_DOWN:
-				vy += vel;
-				break;
-
 			case SDLK_LEFT:
 				vx -= vel;
 				break;
@@ -50,15 +36,67 @@ void obj::handleEvents(SDL_Event& e) {
 	}
 }
 
-void obj::move() {
+void player::move() {
 	x += vx;
-	y += vy;
+
+	if (x <= 0 || x >= screen_width-width) {
+		x -= vx;
+	}
 }
 
-void obj::render(SDL_Renderer* renderer) {
+void player::render(SDL_Renderer* renderer) {
 	SDL_Rect rect = {x,y,width,height};
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+	SDL_RenderFillRect(renderer, &rect);
+}
+
+// Ball class
+class ball {
+	public:
+		int x = screen_width/2;
+		int y = screen_height/2;
+		static const int dim = 30;
+
+		ball();
+		void move();
+		void render(SDL_Renderer* renderer);
+
+	private:
+		int vx;
+		int vy;
+		static const int vel = 1;
+};
+
+ball::ball() {
+	vx = vel;
+	vy = vel;
+}
+
+void ball::move() {
+	x += vx;
+	y += vy;
+
+	if (x <= 0 || x >= screen_width-dim) {
+		vx *= -1;
+	}
+
+	if (y <= 0 || y >= screen_height-dim) {
+		vy *= -1;
+	}
+}
+
+void ball::render(SDL_Renderer* renderer) {
+	SDL_Rect rect = {x,y,dim,dim};
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 	SDL_RenderFillRect(renderer, &rect);
+}
+
+void collision() {
+	if (ball.x >= player.x && ball.x <= player.x-ball.dim && ball.y >= player.y-ball.dim) {
+		printf("Collision");
+		/* vx *= -1; */ 
+		/* vy *= -1; */ 
+	}
 }
 
 int main() {
@@ -73,8 +111,8 @@ int main() {
 	window = SDL_CreateWindow("Pong Game", 
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
-			width,
-			height,
+			screen_width,
+			screen_height,
 			SDL_WINDOW_SHOWN);
 
 	// Check if window is created, report error if not
@@ -93,22 +131,28 @@ int main() {
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
 	// Main loop
+	player player;
+	ball ball;
+
 	SDL_Event e;
 	bool run = true;
-	obj obj;
 	while (run) {
 		while (SDL_PollEvent(&e) != 0) {
 			if (e.type == SDL_QUIT) {
 				run = false;
 			}
-			obj.handleEvents(e);
+			player.handleEvents(e);
 		}
-		obj.move();
+		player.move();
+		ball.move();
 
-		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		collision();
+
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderClear(renderer);
 
-		obj.render(renderer);
+		player.render(renderer);
+		ball.render(renderer);
 
 		SDL_RenderPresent(renderer);
 	}
